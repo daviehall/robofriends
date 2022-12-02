d2a86067469a1285dad6db4e51a5087cdbe13eeb
Dave Hall <davehall@Daves-MBP.askey.com>
2022-12-01 23:47:13 -0800

    changing robofirends to react hooks

---
+++ src/containers/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import CardList from '../components/CardList'
+// import {robots} from './robots'
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
+import ErrorBoundry from '../components/ErrorBoundry'
+import './App.css'
+
+
+function App (){
+    const [robots, setRobots ]= useState([])
+    const [searchField, setSearchField]= useState('')
+    
+    useEffect (() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => setRobots(users))
+    }, [])
+
+    const onSearchChange = (event) => {
+        return setSearchField(event.target.value)
+    }
+
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+    
+    return !robots.length ?
+        <h1 className='tc'>Loading </h1> :
+        (
+            <div className='tc'>
+                <h1 className='f2'>RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange} />
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList  robots={filteredRobots}/>
+                    </ErrorBoundry>
+                </Scroll>
+            </div >
+        )
+    
+}
+    // }
+
+
+// }
+
+export default App
